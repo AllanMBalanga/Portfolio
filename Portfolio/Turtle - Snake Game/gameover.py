f41811964d181f3e12522d2c8f056ca8a8400dd1
AllanMBalanga <92279012+AllanMBalanga@users.noreply.github.com>
2025-03-12 20:34:12 +0800

    Delete Portfolio directory

--- Portfolio/Turtle - Snake Game/gameover.py
+++
@@ -1,8 +0,0 @@
-from turtle import Turtle
-ALIGN = "center"
-FONT = ("Arial", 15, "normal")
-class GameOver(Turtle):
-    def __init__(self):
-        super().__init__()
-        self.color("white")
-        self.write("Game Over", align=ALIGN, font=FONT)
